e7cc0498f0fa40860be860ca313d767ae09f3a96
Martin <miles@martin.by>
2020-09-16 16:17:58 +0100

    Added Get-SifonPlugins script for cloning other scripts from this repository straight into Sifon

---
+++ Get-SifonPlugins.ps1
@@ -0,0 +1,10 @@
+### Name: Get Sifon plugins
+### Description: Downloads the plugins from Sifon repository (requires git)
+### Compatibility: Sifon 0.95
+
+Write-output "Sifon-MuteOutput"
+Remove-Item -Path Sifon.Plugins -Recurse -Force -Confirm:$false
+git clone https://github.com/MartinMiles/Sifon.Plugins.git
+
+Write-output "Sifon-UnmuteOutput"
+Write-output "Script installed under Plugins menu"
