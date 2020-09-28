7154dd2b6becd07d866ec35f556d4d4a15e25256
Martin <miles@martin.by>
2020-09-28 05:36:20 +0100

    Added a temporal demo script for exposing containers profile plugin-script parameters

---
+++ Containers/Output-ProfileValues.ps1
@@ -0,0 +1,17 @@
+### Name: Output container profile parameters
+### Description: Outputs container profile variables - shows you all the parameters options to use with containers
+### Compatibility: Sifon 0.98
+
+param(
+    [string]$ProfileName,
+    [string]$Repository,
+    [string]$Folder,
+    [string]$AdminPassword,
+    [string]$SaPassword
+)
+
+Write-Output "ProfileName = $ProfileName"
+Write-Output "Repository = $Repository"
+Write-Output "Folder = $Folder"
+Write-Output "AdminPassword = $AdminPassword"
+Write-Output "SaPassword = $SaPassword"
