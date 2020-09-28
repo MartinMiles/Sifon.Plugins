7512f63973dfa6728dd7d64e2b90b4b74e030025
Martin <miles@martin.by>
2020-09-28 21:12:23 +0100

    Roadmap info added

---
+++ Containers/Other/Roadmap.ps1
@@ -0,0 +1,20 @@
+### Name: Roadmap for containers in Sifon
+### Description: Just a basic overview of further plans and thoughts
+### Compatibility: Sifon 0.98
+
+Write-output "_"
+Write-Warning "_    Sifon and containers support"
+Write-output "_"
+Write-output "_    As Sitecore added containers support from version 10.0, Sifon now plans adding certain features to help with containers support."
+Write-output "_"
+Write-output "_    Currently, just very basics have been added to setup several containers configuration and quickly switch between them."
+Write-output "_    This approach follows up existing profile priciples already used in Sifon for switching between profiles."
+Write-output "_ "
+Write-output "_    I am planning to add more usefult features with time, such as inspecting, browsing and tuning containers within a selected profile."
+Write-output "_"
+Write-output "_    Your feedback and wishlist are very velcome!"
+Write-output "_"
+Write-output "_"
+Write-output "#COLOR:GREEN#_    Thank you for using Sifon!"
+
+
