3b2362e5d1910574deb5387467d6ef92e1bd0c70
Martin Miles <miles@martin.by>
2020-09-30 02:56:37 +0100

    Minor improvements into Populate-Schema plugin

--- Solr/Populate-Schema.ps1
+++ Solr/Populate-Schema.ps1
@@ -1,21 +1,30 @@
 ### Name: Populates Managed Schema
 ### Description: Populates Managed Schema for a specifix index or all of them
-### Compatibility: Sifon 0.95, SIF
+### Compatibility: Sifon 0.98, SIF
+### Dependencies: "Populate-Schema.json"
 
 param(
+    [string]$Webroot,
     [string]$IndexName = "sitecore_master_index",
     [string]$AdminUsername,
-    [string]$AdminPassword
+    [string]$AdminPassword,
+    $SelectedFile
 )
 
 [string]$Hostname = Get-InstanceUrl -Webroot $Webroot
 
+$path = $MyInvocation.MyCommand.Definition.Replace(".ps1", ".json")
+
 $params = @{
-    Path            = $MyInvocation.MyCommand.Definition.Replace(".ps1", ".json")
+    Path            = $path 
     Hostname        = $Hostname
     IndexName       = $IndexName
     AdminUsername   = $AdminUsername
-    AdminPassword   = "$AdminPassword"
+    AdminPassword   = $AdminPassword
 }
 
-Install-SitecoreConfiguration  @params
+Write-Output "Populating managed schema for index: $IndexName ..."
+Write-Output "Sifon-MuteOutput"
+    Install-SitecoreConfiguration  @params
+Write-Output "Sifon-UnmuteOutput"
+Write-Output "#COLOR:GREEN# Operation complete."
