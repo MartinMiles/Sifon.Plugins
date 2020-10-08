9ca44403df105279a304d0a2329fb75a967f1803
Martin Miles <miles@martin.by>
2020-10-08 04:06:57 +0100

    Added more to system plugins

--- System/Show-SystemInfo.ps1
+++ System/Show-SystemInfo.ps1
@@ -1,1 +1,14 @@
+### Name: Show system info
+### Description: Shows information about the system for the selected profile
+
+        Write-Output "-----------------------------------------"
+        Write-Warning  "System Information for a Selected Profile"
+        Write-Output "-----------------------------------------"
+        Write-Output "."
+        Write-Output "."
+
+$product = (Get-WmiObject -class Win32_OperatingSystem).Caption
+$build = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").ReleaseId
+Write-Output "$product, build $build"
+Write-Output "."
 "The current machine name is: $env:computername"
