0c6d26b72a1202f39068bfe568ca36cb4febdc15
Martin Miles <miles@martin.by>
2020-10-10 10:05:43 +0100

    Dynamic shared plugin (Version selector)

---
+++ Other/Select-SitecoreVersion.ps1
@@ -0,0 +1,16 @@
+### Name: Version selector
+### Description: Ask user to select Sitecore version
+### Compatibility: Sifon 0.99
+### $SelectedVersion = new Sifon.Shared.Forms.VersionSelectorDialog.VersionSelector::GetVersion("$Webroot\bin\Sitecore.Kernel.dll", "Sitecore version selector", "Please select Sitecore version:", "OK")
+
+param(
+    [string]$Webroot,
+    [string]$Website,
+    [string]$Prefix,
+    [PSCredential]$PortalCredentials,
+    $SelectedVersion
+)
+
+if($SelectedVersion -ne $null){
+    $SelectedVersion.Product
+}
