c200eecf553b083cb3a7352f8a5846e9c347d631
Martin Miles <miles@martin.by>
2020-10-11 01:20:04 +0100

    Another example of profile-agnostic folder browser script plugin added

---
+++ Other/Browse-Folder.ps1
@@ -0,0 +1,10 @@
+### Name: Folder selector
+### Description: Works on both remote and local
+### Compatibility: Sifon 0.99
+### $SelectedFolder = new Sifon.Shared.Forms.FolderBrowserDialog.FolderBrowser::GetFolder($Profile, $true)
+
+param(
+        $SelectedFolder
+)
+
+$SelectedFolder
