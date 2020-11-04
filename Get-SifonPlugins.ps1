3627318d696d623bbde8ffb192f07e7ae313a636
Martin Miles <miles@martin.by>
2020-11-04 15:09:54 +0000

    Better version of Get-SifonPlugins.ps1

--- Get-SifonPlugins.ps1
+++ Get-SifonPlugins.ps1
@@ -4,7 +4,10 @@
 
 Write-output "Pulling scripts from a community repository on GitHub ..."
 Write-output "Sifon-MuteOutput"
-Remove-Item -Path Sifon.Plugins -Recurse -Force -Confirm:$false
+
+$pluginsFolder = Join-Path (Get-Location) -ChildPath "Sifon.Plugins"
+
+Remove-Item -Path $pluginsFolder -Recurse -Force -Confirm:$false
 git clone https://github.com/MartinMiles/Sifon.Plugins.git
 
 Write-output "Sifon-UnmuteOutput"
