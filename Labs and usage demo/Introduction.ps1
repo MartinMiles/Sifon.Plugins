a5e0f3ddae6fbd3ca82a8cb57bf6465d3400edcb
Martin Miles <miles@martin.by>
2020-11-17 07:08:47 +0000

    Added introduction to Labs

---
+++ Labs and usage demo/Introduction.ps1
@@ -0,0 +1,43 @@
+### Name: Introduction to 'Labs and Demos'
+### Description: A brief overview of what you find under this folder and its purpose - run me first!
+### Compatibility: Sifon 1.01
+### Execution: Local
+
+Write-Output "."
+Show-Message -Fore Yellow -Back White -Text 'Welcome to "Labs and Demos" section'
+Write-Output "."
+Write-Output "."
+Write-Output "This is a subfolder of community public plugins repositories that helps understanding how easy one can create own plugins with PowerShell"
+Write-Output "."
+Write-Output "It contains several examples of scripts which could be (partially) used as a boilerplate".
+Write-Output "No need for digging through documentation, finding the right parameters and the ways of passing them down"
+Write-Output "Just reuse some useful parts of script from this folder and you're done - that indeed saves time!"
+Write-Output "."
+Write-Output "."
+Write-Output "."
+
+# Show-Message -Fore Yellow -Back White -Text 'Contact and references'
+Show-Message -Fore Yellow -Back White -Text "Send author an instant feedback:"
+Write-Output "."
+Write-Output "If you have issues with Sifon or may just want to ask for some help on making a plugin, or probably anything else - welcome!"
+Write-Output "There is a feedback form under 'About' menu - it sends me an instant message"
+Write-Output "."
+Write-Output "."
+Write-Output "."
+
+Show-Message -Fore Yellow -Back White -Text "Community Plugins Repository:"
+Write-Output "."
+Write-Output "This script and the rest of others from under 'Plugins' menu items come from a public repository located at:"
+Write-Output "#COLOR:LIGHTBLUE#https://github.com/MartinMiles/Sifon.Plugins.Example"
+Write-Output "."
+Write-Output "Code from this repo is cloned by 'Get Sifon plugins' script at the top of 'Plugins' folder into Sifon, enhancing its functionality"
+Write-Output "You are very welcome to share you scripts with the rest of community - please send your pull requests!"
+Write-Output "."
+Write-Output "."
+Write-Output "."
+
+Show-Message -Fore Yellow -Back White -Text  "Documentation and Guides:"
+Write-Output "."
+Write-Output "Available from the program website:"
+Write-Output "#COLOR:LIGHTBLUE#https://Sifon.UK"
+Write-Output "#COLOR:GRAY#(right click the URL above to copy it to clipboard)"
