fb62c621f307aa897eb28297df60cea9458f492f
Martin Miles <miles@martin.by>
2020-12-11 11:27:14 +0000

    New script for deleting Solr cores with data

---
+++ Solr/Delete-CoreData.ps1
@@ -0,0 +1,30 @@
+﻿### Name: Delete website Solr core data
+### Description: Delete Solr core data for the website
+### Compatibility: Sifon 1.2.0
+
+param
+(
+    [uri]$Url,
+    $Website,
+    $Prefix
+)
+
+$folder = Find-SolrInstances | Where-Object { $_[0] -eq $Url.Port } | select { $_[2] }
+if($null -ne $folder)
+{
+    $folder = $folder.Psobject.Properties.Value
+
+    $services = Get-Service solr* | Where { $_.Status -eq "Running" }
+    $services | Stop-Service
+    get-childitem -path "$folder\$Prefix*\Data" -recurse | remove-item -force -recurse
+    $services | Start-Service
+
+    $site = Get-WebSite $Website
+    Restart-WebAppPool -Name $site.applicationPool
+
+    Show-Message -fore White -back yellow -text "The data has been deleted for Solr cores prefixed with: $Prefix"
+}
+else{
+    Show-Message -fore Red -back White -text "Failed to identfy running Solr for: $Url"
+}
+
--- Solr/Rebuild-Index.ps1
+++ Solr/Rebuild-Index.ps1
@@ -1,7 +1,6 @@
 ### Name: Rebuild indexes
 ### Description: Rebilds indexes via SPE remoting
 ### Compatibility: Sifon 1.0.1
-### Display: (Non)Local
 ### $Indexes = new Sifon.Shared.Forms.IndexSelectorDialog.IndexSelector::GetIndex($Profile)
 
 param(
