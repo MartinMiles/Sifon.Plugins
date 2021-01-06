2fc9eb2c1c9f4301f7a66d3b8c0e3bd823071f04
Martin Miles <miles@martin.by>
2021-01-06 00:17:14 +0000

    Added ready plugin for creating Solr Cores for SXA indexes

--- Solr/Create-CoresSXA.ps1
+++ Solr/Create-CoresSXA.ps1
@@ -2,35 +2,76 @@
 ### Description: Create cores for SXA built-in indexes: ***_sxa_master_index and ***_sxa_web_index
 ### Compatibility: Sifon 1.2.3
 
-param
-(
-    [uri]$Solr,
-    $Prefix
+param(
+    [string]$Webroot,
+    [string]$Website,
+    [string]$Prefix,
+    [string]$AdminUsername,
+    [string]$AdminPassword,
+    [uri]$Solr
 )
-    
-$folder = Find-SolrInstances | Where-Object { $_[0] -eq $Solr.Port } | select { $_[2] }
+
+Show-Progress -Percent 2 -Activity "Identifying Solr instances installed" 
+
+Write-Output "Sifon-MuteProgress"
+$instances = Find-SolrInstances 
+$version = $instances | Where-Object { $_[0] -eq $Solr.Port } | select { $_[1] }
+$folder = $instances | Where-Object { $_[0] -eq $Solr.Port } | select   { $_[2] }
+Write-Output "Sifon-UnmuteProgress"
+
 if($null -ne $folder)
 {
     $folder = $folder.Psobject.Properties.Value
+    $version = $version.Psobject.Properties.Value    
+
+    if($version -match "@{solr-spec-version=(.*)}"){
+        $version = $matches[1]
+    }
+    else{
+        Show-Message -Fore Red -Back Yellow -Text "Cannot identify Solr version. Terminating..."
+        exit
+    }
+
     if($null -ne $folder)
     {
+        Import-Module SitecoreInstallFramework -Force
+        Show-Progress -Percent 31 -Activity "Now installing SXA Solr Cores" 
         "."
-        # TODO: Verify if folder exists, notify for local or delete data without creating a folder
-        Copy-Item "$folder\${Prefix}_master_index" "$folder\${Prefix}_sxa_master_index" -recurse
-        Remove-Item -Recurse -Force "$folder\${Prefix}_sxa_master_index\data"
-        Remove-Item -Force "$folder\${Prefix}_sxa_master_index\core.properties"
-        "Created core: ${Prefix}_sxa_master_index"
+        $sitewebroot = $Webroot
+        $PathToJson =  Split-Path -Path $PSCommandPath -Parent
+        $SolrUrl = $Solr
+        $SolrRoot = $folder.Replace("\server\solr","")
+        $SolrService = "solr-$version"
+        $SolrCorePrefix = $Prefix
+        $SitecoreSiteName = $Website
+        $SitecoreAdminPassword = $AdminPassword
+        $sitewebroot = $Webroot
 
-        Copy-Item "$folder\${Prefix}_web_index" "$folder\${Prefix}_sxa_web_index" -recurse
-        Remove-Item -Recurse -Force "$folder\${Prefix}_sxa_web_index\data"
-        Remove-Item -Force "$folder\${Prefix}_sxa_web_index\core.properties"
-        "Created core: ${Prefix}_sxa_web_index"
+        $sxaIndexCreateParams = @{
+            Path                  = "$PathToJson\Create-Cores-SXA-solr.json"
+            SolrUrl               = $SolrUrl
+            SolrRoot              = $SolrRoot
+            SolrService           = $SolrService
+            CorePrefix            = $SolrCorePrefix
+            SiteName              = $SitecoreSiteName
+            SitecoreAdminPassword = $SitecoreAdminPassword
+            SiteWebRootPath       = $sitewebroot
+            SuggesterJsonPath     = "$PathToJson\Create-Cores-SXA-config.json"
+        }
 
+        Write-Output "Sifon-MuteOutput"
+        Write-Output "Sifon-MuteProgress"
+            Install-SitecoreConfiguration @sxaIndexCreateParams
+            # can also do: Uninstall-SitecoreConfiguration @sxaIndexCreateParams
+        Write-Output "Sifon-UnmuteProgress"        
+        Write-Output "Sifon-UnmuteOutput"
+
+        Show-Message -fore white -back yellow -Text "SXA Solr Cores have been added into $folder"
         "."
-        Show-Message -fore White -back yellow -text "SXA Solr cores created. Now you may want populating managed schema followed by rebuilding SXA indexes"
     }
 }
-    $services = Get-Service solr* | Where { $_.Status -eq "Running" }
-    $services | Stop-Service
-    get-childitem -path "$folder\$Prefix*\Data" -recurse | remove-item -force -recurse
-    $services | Start-Service
+else{
+    Show-Message -Fore Red -Back Yellow -Text "Failed to edintify Solr folder. Terminating..."
+}
+
+Show-Progress -Percent 100 -Activity "Finished" 
