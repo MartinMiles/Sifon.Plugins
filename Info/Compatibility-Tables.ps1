5ffd3904cc039838fe8627a638a846d53e67cb71
Martin Miles <miles@martin.by>
2022-01-22 03:20:30 +0000

    Added compatibility tables info

---
+++ Info/Compatibility-Tables.ps1
@@ -0,0 +1,30 @@
+### Name: Compatibility tables
+### Description: This info helps you decide what modules are suitable for your Sitecore instance
+### Compatibility: Sifon 1.2.5       
+       
+"."
+"Sitecore compatibility tables"
+"."
+$Message  = @(
+    "The Sitecore 9+ compatibility table",
+    "https://support.sitecore.com/kb?id=kb_article_view&sysparm_article=KB0087164"
+);
+Show-Message -Fore white -Back yellow -Text $Message
+"."
+$Message  = @(
+    "Sitecore Experience Accelerator compatibility tables",
+    "https://support.sitecore.com/kb?id=kb_article_view&sysparm_article=KB0180187"
+);
+Show-Message -Fore white -Back yellow -Text $Message
+"."
+$Message  = @(
+    "Sitecore JSS Application Mode Compatibility Table (unofficial)",
+    "http://amitkumarmca04.blogspot.com/2020/03/sitecore-jss-application-mode.html"
+);
+Show-Message -Fore white -Back yellow -Text $Message
+"."
+$Message  = @(
+    "Sitecore Publishing Service - compatibility tables (for SPS 4.1.0 and later)",
+    "https://support.sitecore.com/kb?id=kb_article_view&sysparm_article=KB0761308"
+);
+Show-Message -Fore white -Back yellow -Text $Message
